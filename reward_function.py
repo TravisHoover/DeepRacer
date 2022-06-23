b70ebc771c1e7a4acd56638b4db48eb370eb9fab
Travis Hoover <thoover@fortresstech.net>
2022-06-22 17:25:59 -0700

    Advanced reward function that penalizes excessive steering and promotes centerline following

--- reward_function.py
+++ reward_function.py
@@ -1,25 +1,28 @@
 def reward_function(params):
     '''
-    Example of rewarding the agent to follow center line
-    '''
+	Example that penalizes steering, which helps mitigate zig-zag behaviors
+	'''
 
-    # Read input parameters
-    track_width = params['track_width']
-    distance_from_center = params['distance_from_center']
-
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    # Calculate 3 marks that are farther and father away from the center line
+    marker_1 = 0.1 * params['track_width']
+    marker_2 = 0.25 * params['track_width']
+    marker_3 = 0.5 * params['track_width']
 
     # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1.0
-    elif distance_from_center <= marker_2:
+    if params['distance_from_center'] <= marker_1:
+        reward = 1
+    elif params['distance_from_center'] <= marker_2:
         reward = 0.5
-    elif distance_from_center <= marker_3:
+    elif params['distance_from_center'] <= marker_3:
         reward = 0.1
     else:
         reward = 1e-3  # likely crashed/ close to off track
+
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15
+
+    # Penalize reward if the car is steering too much
+    if abs(params['steering_angle']) > ABS_STEERING_THRESHOLD:  # Only need the absolute steering angle
+        reward *= 0.5
 
     return float(reward)

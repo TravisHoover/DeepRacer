417082418417bf1836ed0f501367a509050fd2be
Travis Hoover <thoover@fortresstech.net>
2022-06-22 17:27:13 -0700

    Advanced reward function that penalizes slow driving. This creates a non-linear reward function so it may take longer to learn.

--- reward_function.py
+++ reward_function.py
@@ -1,8 +1,4 @@
 def reward_function(params):
-    '''
-	Example that penalizes steering, which helps mitigate zig-zag behaviors
-	'''
-
     # Calculate 3 marks that are farther and father away from the center line
     marker_1 = 0.1 * params['track_width']
     marker_2 = 0.25 * params['track_width']
@@ -18,11 +14,11 @@ def reward_function(params):
     else:
         reward = 1e-3  # likely crashed/ close to off track
 
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
-
-    # Penalize reward if the car is steering too much
-    if abs(params['steering_angle']) > ABS_STEERING_THRESHOLD:  # Only need the absolute steering angle
+    # penalize reward for the car taking slow actions
+    # speed is in m/s
+    # we penalize any speed less than 0.5m/s
+    SPEED_THRESHOLD = 0.5
+    if params['speed'] < SPEED_THRESHOLD:
         reward *= 0.5
 
     return float(reward)
